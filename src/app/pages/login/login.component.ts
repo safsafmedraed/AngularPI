af43a3512115ad413a9a3d164d47dd3e1f089b76
dheker <dheker.benamor@esprit.tn>
2019-11-27 15:10:18 +0100

    login

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit, OnDestroy} from '@angular/core';
+import {Component, OnInit, OnDestroy, Inject} from '@angular/core';
 import {Entreprise} from '../../Models/entreprise';
 import {ActivatedRoute, Router} from '@angular/router';
 import {HttpClient} from '@angular/common/http';
@@ -6,6 +6,8 @@ import * as $ from 'jquery';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {first} from 'rxjs/operators';
 import {EntrepriseService} from '../../Services/entreprise.service';
+import {LoginService} from '../../Services/login.service';
+import {LOCAL_STORAGE, WebStorageService} from 'ngx-webstorage-service';
 
 @Component({
   selector: 'app-login',
@@ -23,7 +25,7 @@ export class LoginComponent implements OnInit, OnDestroy {
 
   constructor(private formBuilder: FormBuilder,
               private route: ActivatedRoute,
-              private router: Router, private httpClient: HttpClient, private entrepriseservice: EntrepriseService) {
+              private router: Router, private httpClient: HttpClient, private entrepriseservice: EntrepriseService, private ls: LoginService, @Inject(LOCAL_STORAGE) private storage: WebStorageService) {
   }
 
   ngOnInit() {
@@ -75,6 +77,45 @@ export class LoginComponent implements OnInit, OnDestroy {
           console.log('bad credentials');
         }
       });
+  }
+
+  dashbord(email, password) {
+    this.ls.logIn(email.value, password.value).subscribe(
+      data => {
+
+        if (data.type == 'staff') {
+          console.log('staff');
+
+          this.storage.set('email', email);
+          this.storage.set('type', 'staff');
+          this.storage.set('user', data.user);
+          this.storage.set('token', data.token);
+          this.router.navigateByUrl('dashboard');
+        } else if (data.type == 'student') {
+          console.log('student');
+          this.storage.set('type', 'student');
+          this.storage.set('email', email.value);
+          this.storage.set('user', data.user);
+          this.storage.set('token', data.token);
+          console.log(this.storage.get('user').id);
+          this.router.navigateByUrl('dashboard');
+
+        } else if (data.type == 'encadreur') {
+          console.log('encadreur');
+
+          console.log(email.value);
+          this.storage.set('encadreur', 'encadreur');
+
+          this.storage.set('user', data.user);
+          this.storage.set('email', email.value);
+          this.storage.set('token', data.token);
+          this.router.navigateByUrl('dashboard');
+
+        }
+
+      }
+    );
+
   }
 }
 
